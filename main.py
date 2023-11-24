2314348a872a3a59afc45233b707457f2e0d25c3
ArtemAdd <artm.baranov.08@gmail.com>
2023-11-24 17:53:31 +0500

    Прямоугольник

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     pygame.init()
     size = width, height = list(map(lambda x: int(x), input().split(' ')))
     screen = pygame.display.set_mode(size)
-    pygame.draw.line(screen, 'white', (0, 0), size, 5)
-    pygame.draw.line(screen, 'white', (width, 0), (0, height), 5)
+    screen.fill('black')
+    pygame.draw.rect(screen, 'red', (1, 1, width - 2, height - 2))
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
