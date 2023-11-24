321b7194e32e97b9ebce5de35754a75cb9b15329
ArtemAdd <artm.baranov.08@gmail.com>
2023-11-24 18:13:40 +0500

    Шахматная клетка

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import pygame
 
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = list(map(lambda x: int(x), input().split(' ')))
-    screen = pygame.display.set_mode(size)
-    screen.fill('black')
-    pygame.draw.rect(screen, 'red', (1, 1, width - 2, height - 2))
+    w, n = list(map(lambda x: int(x), input().split(' ')))
+    screen = pygame.display.set_mode((w, w))
+    screen.fill('white')
+    s = w / n
+    flag = True
+    for i in range(n):
+        for j in range(n):
+            if flag:
+                pygame.draw.rect(screen, 'black', (i * s, j * s, s, s))
+                flag = False
+            else:
+                flag = True
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
