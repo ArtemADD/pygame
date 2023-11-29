d767bfef783a040dd7b5b28264f56f18d1386b69
ArtemAdd <artm.baranov.08@gmail.com>
2023-11-29 15:48:22 +0500

    Я слежу за тобой!

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 import pygame as pg
 
-LEFT_UP = 1
-LEFT_DOWN = 2
-RIGHT_DOWN = 3
-RIGHT_UP = 4
-
 
 class Game:
     def __init__(self):
         pg.init()
-        pg.display.set_caption('Перетаскивание')
-        self.size = self.width, self.height = 300, 300
+        pg.display.set_caption('Я слежу за тобой!')
+        self.size = self.width, self.height = 200, 200
         self.screen = pg.display.set_mode(self.size)
         self.screen.fill('black')
         self.display = pg.display
-        self.v = 5
-        self.balls = []
-        self.fps = 30
-        self.clock = pg.time.Clock()
-        self.rect = Rect(self.screen)
-        self.grap = False
+        self.num = Number(self)
         self.run()
         pg.quit()
 
@@ -29,42 +19,27 @@ class Game:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-                if event.type == pg.MOUSEBUTTONDOWN:
-                    if event.button == pg.BUTTON_LEFT:
-                        if (event.pos[0] in range(self.rect.x, self.rect.x + self.rect.w)
-                                and event.pos[1] in range(self.rect.y, self.rect.y + self.rect.w)):
-                            self.rect.pos_grap = event.pos
-                            self.grap = True
-                if event.type == pg.MOUSEBUTTONUP:
-                    self.rect.x, self.rect.y = self.rect.pos
-                    self.grap = False
-                if event.type == pg.MOUSEMOTION and self.grap:
-                    self.rect.move(event.pos)
+                if event.type == pg.WINDOWFOCUSLOST:
+                    self.num.score += 1
             self.screen.fill('black')
             self.draw()
-            self.clock.tick(self.fps)
             self.display.flip()
 
     def draw(self):
-        self.rect.draw()
+        self.num.draw()
 
 
-class Rect:
-    def __init__(self, screen):
-        self.screen = screen
-        self.pos = 100, 100
-        self.x, self.y = self.pos
-        self.w = 100
-        self.pos_grap = [0, 0]
-
-    def move(self, pos):
-        x, y = pos
-        x -= self.pos_grap[0]
-        y -= self.pos_grap[1]
-        self.pos = [self.x + x, self.y + y]
+class Number:
+    def __init__(self, game):
+        self.screen = game.screen
+        self.game = game
+        self.score = 1
+        self.font = pg.font.Font(None, 100)
 
     def draw(self):
-        pg.draw.rect(self.screen, 'green', (*self.pos, self.w, self.w), border_radius=15)
+        text = self.font.render(str(self.score), True, 'red')
+        pos = self.game.width // 2 - text.get_width() // 2, self.game.height // 2 - text.get_height() // 2
+        self.screen.blit(text, pos)
 
 
 if __name__ == '__main__':
