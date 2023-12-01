66a081a58306eff7e498f0064699d1b1196543f2
ArtemAdd <artm.baranov.08@gmail.com>
2023-12-01 18:48:45 +0500

    Пра-пра-пра-крестики-нолики

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ RIGHT_UP = 4
 class Game:
     def __init__(self):
         pg.init()
-        pg.display.set_caption('Инициализация игры')
+        pg.display.set_caption('Пра-пра-пра-крестики-нолики')
         self.size = self.width, self.height = 501, 501
         self.screen = pg.display.set_mode(self.size)
         self.screen.fill('black')
@@ -38,17 +38,15 @@ class Game:
 
 
 class Board:
-    # создание поля
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.board = [[0] * width for _ in range(height)]
-        # значения по умолчанию
         self.left = 10
         self.top = 10
         self.cell_size = 30
+        self.hod = 0
 
-    # настройка внешнего вида
     def set_view(self, left, top, cell_size):
         self.left = left
         self.top = top
@@ -58,11 +56,23 @@ class Board:
         for i in range(self.width):
             for j in range(self.height):
                 if self.board[j][i] == 0:
-                    pg.draw.rect(screen, 'white', (self.left + (self.cell_size * i), self.top + (self.cell_size * j),
-                                                   self.cell_size, self.cell_size), 1)
-                else:
-                    pg.draw.rect(screen, 'white', (self.left + (self.cell_size * i), self.top + (self.cell_size * j),
-                                                   self.cell_size, self.cell_size), )
+                    pg.draw.rect(screen, 'black', (self.left + (self.cell_size * i), self.top + (self.cell_size * j),
+                                                   self.cell_size, self.cell_size))
+                elif self.board[j][i] == 1:
+                    pg.draw.line(screen, 'red', (self.left + (self.cell_size * i) + 2,
+                                 self.top + (self.cell_size * j) + 2),
+                                 (self.left + (self.cell_size * (i + 1)) - 4,
+                                 self.top + (self.cell_size * (j + 1)) - 4), 1)
+                    pg.draw.line(screen, 'red', (self.left + (self.cell_size * (i + 1)) - 4,
+                                 self.top + (self.cell_size * j) + 2),
+                                 (self.left + (self.cell_size * i) + 2,
+                                 self.top + (self.cell_size * (j + 1)) - 4), 1)
+                elif self.board[j][i] == 2:
+                    pg.draw.ellipse(screen, 'blue', (self.left + (self.cell_size * i) + 2,
+                                    self.top + (self.cell_size * j) + 2,
+                                    self.cell_size - 4, self.cell_size - 4), 1)
+                pg.draw.rect(screen, 'white', (self.left + (self.cell_size * i), self.top + (self.cell_size * j),
+                             self.cell_size, self.cell_size), 1)
 
     def get_cell(self, mouse_pos):
         for i in range(self.width):
@@ -75,7 +85,16 @@ class Board:
         return None
 
     def on_click(self, cell_coords):
-        print(cell_coords)
+        x = cell_coords[0]
+        y = cell_coords[1]
+        if self.board[y][x] != 0:
+            pass
+        elif self.hod == 0:
+            self.hod = 1
+            self.board[y][x] = 1
+        elif self.hod == 1:
+            self.hod = 0
+            self.board[y][x] = 2
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
