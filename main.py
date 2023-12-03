c3fe50a95c00125a253cd1b5b20ce8e87690f772
ArtemAdd <artm.baranov.08@gmail.com>
2023-12-03 15:43:52 +0500

    Куб

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import pygame as pg
 
 class Game:
     def __init__(self):
+        self.set_cube()
         pg.init()
-        pg.display.set_caption('Кирпичи')
-        self.size = self.width, self.height = 300, 200
+        pg.display.set_caption('Куб')
+        self.size = self.width, self.height = 300, 300
         self.screen = pg.display.set_mode(self.size)
-        self.screen.fill('white')
+        self.screen.fill('black')
         self.display = pg.display
-        self.bricks = Bricks(self)
+        self.cube = Cube(self)
         self.run()
         pg.quit()
 
@@ -19,39 +20,50 @@ class Game:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-            self.screen.fill('white')
+            self.screen.fill('black')
             self.draw()
             self.display.flip()
 
     def draw(self):
-        self.bricks.draw(self.screen)
+        self.cube.draw(self.screen)
+
+    def set_cube(self):
+        n = input().split()
+        if int(n[0]) > 100 or int(n[0]) % 4 != 0 or int(n[1]) > 360:
+            print('Неправильный формат ввода')
+            pg.quit()
+        self.w = int(n[0])
+        self.c = int(n[1])
 
 
-class Bricks:
+class Cube:
     def __init__(self, game):
+        self.w = game.w
         self.width = game.width
         self.height = game.height
-        self.h = 15
-        self.w = 30
+        c_1 = pg.Color(0, 0, 0)
+        c_2 = pg.Color(0, 0, 0)
+        c_3 = pg.Color(0, 0, 0)
+        c_1.hsva = (game.c, 100, 100, 100)
+        self.c_1 = c_1
+        c_2.hsva = (game.c, 100, 75, 100)
+        self.c_2 = c_2
+        c_3.hsva = (game.c, 100, 50, 100)
+        self.c_3 = c_3
 
     def draw(self, screen):
-        q = self.width // self.w + 1
-        k = self.height // self.h + 1
-        for j in range(k):
-            for i in range(q):
-                if j == 0:
-                    pg.draw.rect(screen, 'red', ((self.w * i) + (i * 2), self.h * j, self.w, self.h))
-                    continue
-                if j % 2 == 0 and j != 0:
-                    pg.draw.rect(screen, 'red', ((self.w * i) + (i * 2), self.h * j + (j * 2), self.w, self.h))
-                else:
-                    if i == 0:
-                        pg.draw.rect(screen, 'red', (0, self.h * j + (j * 2), self.w // 2, self.h))
-                        pg.draw.rect(screen, 'red', ((self.w // 2) + (self.w * i) + (1 * 2),
-                                                     self.h * j + (j * 2), self.w, self.h))
-                    else:
-                        pg.draw.rect(screen, 'red', ((self.w // 2) + (self.w * i) + ((i + 1) * 2),
-                                                     self.h * j + (j * 2), self.w, self.h))
+        pg.draw.rect(screen, self.c_2, (self.width // 2 - self.w // 2, self.height // 2,
+                                        self.w, self.w))
+        pg.draw.polygon(screen, self.c_1, [(self.width // 2 - self.w // 2, self.height // 2),
+                                           (self.width // 2 + self.w // 2, self.height // 2),
+                                           (self.width // 2 + self.w, self.height // 2 - self.w // 2),
+                                           (self.width // 2, self.height // 2 - self.w // 2)
+                                           ])
+        pg.draw.polygon(screen, self.c_3, [(self.width // 2 + self.w // 2, self.height // 2),
+                                           (self.width // 2 + self.w, self.height // 2 - self.w // 2),
+                                           (self.width // 2 + self.w, self.height // 2 + self.w // 2),
+                                           (self.width // 2 + self.w // 2, self.height // 2 + self.w)
+                                           ])
 
 
 if __name__ == '__main__':
