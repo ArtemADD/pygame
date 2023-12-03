297d71aa9f5224623e6eacd08fb8a9fb6a1bdcf1
ArtemAdd <artm.baranov.08@gmail.com>
2023-12-03 14:29:57 +0500

    Ромбики

--- main.py
+++ main.py
@@ -9,18 +9,14 @@ LINE = 5
 
 class Game:
     def __init__(self):
-        n = input()
-        if ',' in n:
-            print('Неправильный формат ввода')
-            return
-        self.n = int(n)
+        self.set_n()
         pg.init()
-        pg.display.set_caption('Сфера')
+        pg.display.set_caption('Ромбики')
         self.size = self.width, self.height = 300, 300
         self.screen = pg.display.set_mode(self.size)
-        self.screen.fill('black')
+        self.screen.fill('yellow')
         self.display = pg.display
-        self.circle = Circle(self)
+        self.loz = Lozenges(self)
         self.run()
         pg.quit()
 
@@ -30,28 +26,39 @@ class Game:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-            self.screen.fill('black')
+            self.screen.fill('yellow')
             self.draw()
             self.display.flip()
 
     def draw(self):
-        self.circle.draw(self.screen)
+        self.loz.draw(self.screen)
+
+    def set_n(self):
+        n = input()
+        if ',' in n:
+            print('Неправильный формат ввода')
+            pg.quit()
+        self.n = int(n)
 
 
-class Circle:
+class Lozenges:
     def __init__(self, game):
         self.n = game.n
         self.width = game.width
         self.height = game.height
-        self.half_width = game.width // 2
-        self.half_height = game.height // 2
 
     def draw(self, screen):
-        q = self.width // self.n
-        for i in range(1, self.n + 1):
-            pg.draw.ellipse(screen, 'white', (0, self.half_height - (q * i // 2), self.width, q * i), 1)
-        for i in range(1, self.n + 1):
-            pg.draw.ellipse(screen, 'white', (self.half_width - (q * i // 2), 0, q * i, self.height), 1)
+        k = self.width // self.n
+        q = self.height // self.n
+        half_n = self.n // 2
+        for i in range(k):
+            for j in range(q):
+                pg.draw.polygon(screen, 'orange', [
+                    (half_n + self.n * i, self.n * j),
+                    (self.n * i, half_n + self.n * j),
+                    (half_n + self.n * i, self.n + self.n * j),
+                    (self.n + self.n * i, half_n + self.n * j)
+                ])
 
 
 if __name__ == '__main__':
