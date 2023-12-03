bf6ab42085bc65ae1ba4b68010694c9274297398
ArtemAdd <artm.baranov.08@gmail.com>
2023-12-03 15:07:23 +0500

    Кирпичи

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 import pygame as pg
 
-RIGHT_UP = 1
-LEFT_UP = 2
-LEFT_DOWN = 3
-RIGHT_DOWN = 4
-LINE = 5
-
 
 class Game:
     def __init__(self):
-        self.set_n()
         pg.init()
-        pg.display.set_caption('Ромбики')
-        self.size = self.width, self.height = 300, 300
+        pg.display.set_caption('Кирпичи')
+        self.size = self.width, self.height = 300, 200
         self.screen = pg.display.set_mode(self.size)
-        self.screen.fill('yellow')
+        self.screen.fill('white')
         self.display = pg.display
-        self.loz = Lozenges(self)
+        self.bricks = Bricks(self)
         self.run()
         pg.quit()
 
@@ -26,39 +19,39 @@ class Game:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-            self.screen.fill('yellow')
+            self.screen.fill('white')
             self.draw()
             self.display.flip()
 
     def draw(self):
-        self.loz.draw(self.screen)
-
-    def set_n(self):
-        n = input()
-        if ',' in n:
-            print('Неправильный формат ввода')
-            pg.quit()
-        self.n = int(n)
+        self.bricks.draw(self.screen)
 
 
-class Lozenges:
+class Bricks:
     def __init__(self, game):
-        self.n = game.n
         self.width = game.width
         self.height = game.height
+        self.h = 15
+        self.w = 30
 
     def draw(self, screen):
-        k = self.width // self.n
-        q = self.height // self.n
-        half_n = self.n // 2
-        for i in range(k):
-            for j in range(q):
-                pg.draw.polygon(screen, 'orange', [
-                    (half_n + self.n * i, self.n * j),
-                    (self.n * i, half_n + self.n * j),
-                    (half_n + self.n * i, self.n + self.n * j),
-                    (self.n + self.n * i, half_n + self.n * j)
-                ])
+        q = self.width // self.w + 1
+        k = self.height // self.h + 1
+        for j in range(k):
+            for i in range(q):
+                if j == 0:
+                    pg.draw.rect(screen, 'red', ((self.w * i) + (i * 2), self.h * j, self.w, self.h))
+                    continue
+                if j % 2 == 0 and j != 0:
+                    pg.draw.rect(screen, 'red', ((self.w * i) + (i * 2), self.h * j + (j * 2), self.w, self.h))
+                else:
+                    if i == 0:
+                        pg.draw.rect(screen, 'red', (0, self.h * j + (j * 2), self.w // 2, self.h))
+                        pg.draw.rect(screen, 'red', ((self.w // 2) + (self.w * i) + (1 * 2),
+                                                     self.h * j + (j * 2), self.w, self.h))
+                    else:
+                        pg.draw.rect(screen, 'red', ((self.w // 2) + (self.w * i) + ((i + 1) * 2),
+                                                     self.h * j + (j * 2), self.w, self.h))
 
 
 if __name__ == '__main__':
