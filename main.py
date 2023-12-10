390196bc7ba787e0bd5c8953e98b624ed726e161
ArtemAdd <artm.baranov.08@gmail.com>
2023-12-10 13:44:28 +0500

    Game over

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ class Game:
     def __init__(self):
         pg.init()
         pg.display.set_caption('Boom them all')
-        self.size = self.width, self.height = 500, 500
+        self.size = self.width, self.height = 600, 300
         self.screen = pg.display.set_mode(self.size)
         self.display = pg.display
-        self.screen.fill('black')
+        self.screen.fill('blue')
         self.fps = 100
+        self.v = 5
         self.clock = pg.time.Clock()
         self.sprites = pg.sprite.Group()
-        [Bomb(self, self.sprites) for i in range(20)]
+        self.game_over = GameOver(self, self.sprites)
         self.run()
         pg.quit()
 
@@ -23,34 +24,27 @@ class Game:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-                self.sprites.update(event)
-            self.screen.fill('black')
+            self.screen.fill('blue')
+            self.game_over.do()
             self.sprites.draw(self.screen)
             self.clock.tick(self.fps)
             self.display.flip()
 
 
-class Bomb(pg.sprite.Sprite):
-    image_bomb = pg.image.load('data//bomb.png')
-    image_boom = pg.image.load('data//boom.png')
+class GameOver(pg.sprite.Sprite):
+    image = pg.image.load('data//gameover.png')
 
     def __init__(self, game, *group):
         super().__init__(*group)
         self.game = game
-        self.image = Bomb.image_bomb
-        self.image_boom = Bomb.image_boom
-        self.rect = self.image_bomb.get_rect()
-        self.r_boom = self.image_boom.get_rect()
-        self.rect.x = randint(0, self.game.width - self.rect.w)
-        self.rect.y = randint(0, self.game.height - self.rect.h)
-        self.r_boom.x = self.rect.x - ((self.r_boom.w - self.rect.w) / 2)
-        self.r_boom.y = self.rect.y - ((self.r_boom.h - self.rect.h) / 2)
+        self.image = GameOver.image
+        self.rect = self.image.get_rect()
+        self.rect.x = -self.rect.w
+        self.rect.y = 0
 
-    def update(self, *args):
-        if args and args[0].type == pg.MOUSEBUTTONDOWN and \
-                self.rect.collidepoint(args[0].pos):
-            self.rect = self.r_boom
-            self.image = self.image_boom
+    def do(self):
+        if self.rect.x != 0:
+            self.rect.x += self.game.v
 
 
 if __name__ == '__main__':
