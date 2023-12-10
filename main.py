abaac93ff35d0ac7a47ed9e920287e1061930c87
ArtemAdd <artm.baranov.08@gmail.com>
2023-12-10 12:56:24 +0500

    Герой двигается!

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import pygame as pg
 class Game:
     def __init__(self):
         pg.init()
-        pg.display.set_caption('Курсор')
-        pg.mouse.set_visible(False)
-        self.size = self.width, self.height = 500, 500
+        pg.display.set_caption('Герой двигается!')
+        self.size = self.width, self.height = 300, 300
         self.screen = pg.display.set_mode(self.size)
         self.display = pg.display
-        self.screen.fill('black')
-        self.fps = 100
+        self.screen.fill('white')
+        self.fps = 0
+        self.v = 1
+        self.delta_time = 1
         self.clock = pg.time.Clock()
         self.sprites = pg.sprite.Group()
-        self.cur = Curser(self.sprites)
+        self.hero = Hero(self, self.size, self.sprites)
         self.run()
         pg.quit()
 
@@ -23,26 +24,54 @@ class Game:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-                if event.type == pg.MOUSEMOTION:
-                    self.cur.update()
-            self.screen.fill('black')
-            if pg.mouse.get_focused():
-                self.sprites.draw(self.screen)
-            self.clock.tick(self.fps)
+            self.hero.move()
+            self.screen.fill('white')
+            self.sprites.draw(self.screen)
+            self.delta_time = self.clock.tick(self.fps)
             self.display.flip()
 
 
-class Curser(pg.sprite.Sprite):
-    image = pg.image.load('data//arrow.png')
+class Hero(pg.sprite.Sprite):
+    image = pg.image.load('data//creature.png')
 
-    def __init__(self, *group):
+    def __init__(self, game, size, *group):
         super().__init__(*group)
-        self.image = Curser.image
+        self.game = game
+        self.image = Hero.image
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = 50, 50
+        self.rect.x, self.rect.y = (size[0] // 2) - (self.rect.width // 2), (size[1] // 2) - (self.rect.height // 2)
 
-    def update(self):
-        self.rect.x, self.rect.y = pg.mouse.get_pos()
+    def check_pos(self, keys):
+        x = self.rect.x
+        y = self.rect.y
+        w = self.rect.w
+        h = self.rect.h
+        speed = self.game.v * self.game.delta_time
+        if keys[pg.K_LEFT]:
+            if x - speed < 0:
+                return False
+        if keys[pg.K_RIGHT]:
+            if x + speed + w > self.game.width:
+                return False
+        if keys[pg.K_UP]:
+            if y - speed < 0:
+                return False
+        if keys[pg.K_DOWN]:
+            if y + speed + h > self.game.height:
+                return False
+        return True
+
+    def move(self):
+        speed = self.game.v * self.game.delta_time
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] and self.check_pos(keys):
+            self.rect.x -= speed
+        if keys[pg.K_RIGHT] and self.check_pos(keys):
+            self.rect.x += speed
+        if keys[pg.K_UP] and self.check_pos(keys):
+            self.rect.y -= speed
+        if keys[pg.K_DOWN] and self.check_pos(keys):
+            self.rect.y += speed
 
 
 if __name__ == '__main__':
