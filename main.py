062306fb9232da6e88f56a7af8917c67fa47f8d7
ArtemAdd <artm.baranov.08@gmail.com>
2023-12-10 13:12:18 +0500

    Машинка

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import pygame as pg
 class Game:
     def __init__(self):
         pg.init()
-        pg.display.set_caption('Герой двигается!')
-        self.size = self.width, self.height = 300, 300
+        pg.display.set_caption('Машинка')
+        self.size = self.width, self.height = 600, 95
         self.screen = pg.display.set_mode(self.size)
         self.display = pg.display
         self.screen.fill('white')
-        self.fps = 0
+        self.fps = 100
         self.v = 1
-        self.delta_time = 1
         self.clock = pg.time.Clock()
         self.sprites = pg.sprite.Group()
         self.hero = Hero(self, self.size, self.sprites)
@@ -27,12 +26,12 @@ class Game:
             self.hero.move()
             self.screen.fill('white')
             self.sprites.draw(self.screen)
-            self.delta_time = self.clock.tick(self.fps)
+            self.clock.tick(self.fps)
             self.display.flip()
 
 
 class Hero(pg.sprite.Sprite):
-    image = pg.image.load('data//creature.png')
+    image = pg.image.load('data//car.png')
 
     def __init__(self, game, size, *group):
         super().__init__(*group)
@@ -40,38 +39,30 @@ class Hero(pg.sprite.Sprite):
         self.image = Hero.image
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = (size[0] // 2) - (self.rect.width // 2), (size[1] // 2) - (self.rect.height // 2)
+        self.m = 0
 
-    def check_pos(self, keys):
+    def check_pos(self):
         x = self.rect.x
-        y = self.rect.y
         w = self.rect.w
-        h = self.rect.h
-        speed = self.game.v * self.game.delta_time
-        if keys[pg.K_LEFT]:
-            if x - speed < 0:
-                return False
-        if keys[pg.K_RIGHT]:
+        speed = self.game.v
+        if self.m == 0:
             if x + speed + w > self.game.width:
+                self.image = pg.transform.flip(self.image, self.rect.x, self.rect.y)
+                self.m = 1
                 return False
-        if keys[pg.K_UP]:
-            if y - speed < 0:
-                return False
-        if keys[pg.K_DOWN]:
-            if y + speed + h > self.game.height:
+        if self.m == 1:
+            if x - speed < 0:
+                self.image = pg.transform.flip(self.image, self.rect.x + self.rect.w, self.rect.y)
+                self.m = 0
                 return False
         return True
 
     def move(self):
-        speed = self.game.v * self.game.delta_time
-        keys = pg.key.get_pressed()
-        if keys[pg.K_LEFT] and self.check_pos(keys):
-            self.rect.x -= speed
-        if keys[pg.K_RIGHT] and self.check_pos(keys):
+        speed = self.game.v
+        if self.m == 0 and self.check_pos():
             self.rect.x += speed
-        if keys[pg.K_UP] and self.check_pos(keys):
-            self.rect.y -= speed
-        if keys[pg.K_DOWN] and self.check_pos(keys):
-            self.rect.y += speed
+        if self.m == 1 and self.check_pos():
+            self.rect.x -= speed
 
 
 if __name__ == '__main__':
