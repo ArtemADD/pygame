b6475ad2fa5005e072abb74dd15f671aa47a1604
ArtemAdd <artm.baranov.08@gmail.com>
2023-12-15 19:46:15 +0500

    Перемещение героя

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import pygame as pg
 from random import randint
 
@@ -5,15 +7,16 @@ from random import randint
 class Game:
     def __init__(self):
         pg.init()
-        pg.display.set_caption('Boom them all — 2')
-        self.size = self.width, self.height = 500, 500
+        pg.display.set_caption('Перемещение героя')
+        self.size = self.width, self.height = 800, 800
         self.screen = pg.display.set_mode(self.size)
         self.display = pg.display
         self.screen.fill('black')
         self.fps = 100
         self.clock = pg.time.Clock()
+        self.map = Map(self)
         self.sprites = pg.sprite.Group()
-        [Bomb(self, self.sprites) for i in range(10)]
+        self.player = Player(self, self.sprites)
         self.run()
         pg.quit()
 
@@ -23,54 +26,68 @@ class Game:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-                self.sprites.update(event)
+                if event.type == pg.KEYDOWN:
+                    self.player.move()
             self.screen.fill('black')
+            self.map.draw()
             self.sprites.draw(self.screen)
-            # [sp.draw() for sp in self.sprites]
             self.clock.tick(self.fps)
             self.display.flip()
 
 
-class Bomb(pg.sprite.Sprite):
-    image_bomb = pg.image.load('data//bomb.png')
-    image_boom = pg.image.load('data//boom.png')
+class Map:
+    def __init__(self, game):
+        self.game = game
+        self.minimap = [[randint(0, 2) for m in range(game.width // 50)] for _ in range(game.height // 50)]
+        self.grass_image = pg.image.load('data//grass.png')
+        self.box_image = pg.image.load('data//box.png')
+
+    def draw(self):
+        for j in range(len(self.minimap)):
+            for i in range(len(self.minimap[j])):
+                if self.minimap[j][i] % 2 == 0:
+                    self.game.screen.blit(self.grass_image, (i * 50, j * 50))
+                else:
+                    self.game.screen.blit(self.box_image, (i * 50, j * 50))
+
+
+class Player(pg.sprite.Sprite):
+    image = pg.transform.scale(pg.image.load('data//mar.png'), (50, 50))
 
     def __init__(self, game, *group):
         super().__init__(*group)
         self.game = game
-        self.image_bomb = Bomb.image_bomb
-        self.image_boom = Bomb.image_boom
-        self.rect = self.image_bomb.get_rect()
-        self.r_boom = self.image_boom.get_rect()
-        self.rect.x = randint(int((self.r_boom.w - self.rect.w) / 2), self.game.width - self.r_boom.w)
-        self.rect.y = randint(int((self.r_boom.h - self.rect.h) / 2), self.game.height - self.r_boom.h)
-        self.image = self.image_boom
-        self.mask = pg.mask.from_surface(self.image)
-        while self.check_collide():
-            self.rect.x = randint(int((self.r_boom.w - self.rect.w) / 2), self.game.width - self.r_boom.w)
-            self.rect.y = randint(int((self.r_boom.h - self.rect.h) / 2), self.game.height - self.r_boom.h)
-        self.r_boom.x = self.rect.x - ((self.r_boom.w - self.rect.w) / 2)
-        self.r_boom.y = self.rect.y - ((self.r_boom.h - self.rect.h) / 2)
-        self.image = self.image_bomb
+        self.image = Player.image
+        self.rect = self.image.get_rect()
+        self.x = randint(0, (game.width // 50) - 1)
+        self.y = randint(0, (game.height // 50) - 1)
+        while self.game.map.minimap[self.y][self.x] == 1:
+            self.x = randint(0, (game.width // 50) - 1)
+            self.y = randint(0, (game.height // 50) - 1)
+        self.rect.x = self.x * 50
+        self.rect.y = self.y * 50
 
-    # def draw(self):
-    #     pg.draw.rect(self.game.screen, 'white', self.rect, 1)
-
-    def update(self, *args):
-        if args and args[0].type == pg.MOUSEBUTTONDOWN and \
-                self.rect.collidepoint(args[0].pos):
-            self.rect = self.r_boom
-            self.image = self.image_boom
-
-    def check_collide(self):
-        checked = list(map(lambda x: True if pg.sprite.collide_mask(self, x) else False, self.game.sprites))
-        print(checked)
-        if len(checked) == 1:
-            return False
-        if any(checked[:-1]) is False:
-            return False
-        return True
+    def move(self):
+        keys = pg.key.get_pressed()
+        x = self.x
+        y = self.y
+        if keys[pg.K_w]:
+            y -= 1
+        elif keys[pg.K_a]:
+            x -= 1
+        elif keys[pg.K_s]:
+            y += 1
+        elif keys[pg.K_d]:
+            x += 1
+        if (0 <= x < len(self.game.map.minimap) and 0 <= y < len(self.game.map.minimap)
+                and self.game.map.minimap[y][x] != 1):
+            print(x, y)
+            self.x = x
+            self.y = y
+            self.rect.x = self.x * 50
+            self.rect.y = self.y * 50
 
 
 if __name__ == '__main__':
     game = Game()
+    sys.exit()
